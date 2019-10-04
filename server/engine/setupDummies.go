c8eca19624772fdcd3a73ba064ad8b739849e31e
Bartosz <bartek.burgiel@hotmail.com>
2019-10-04 21:32:18 +0200

    Add keyed fields to setupDummies()

--- server/engine/setupDummies.go
+++ server/engine/setupDummies.go
@@ -7,22 +7,22 @@ import "kanbanBoard/server/templates"
 func setupDummies() templates.Tasks {
 
 	toDoTickets := templates.Tickets{
-		templates.Ticket{"Ticket1", "Description of the task 1", 1},
-		templates.Ticket{"Ticket2", "Description of the task 2", 2},
-		templates.Ticket{"Ticket3", "Description of the task 3", 3},
+		templates.Ticket{Title: "Ticket1", Description: "Description of the task 1", ID: 1},
+		templates.Ticket{Title: "Ticket2", Description: "Description of the task 2", ID: 2},
+		templates.Ticket{Title: "Ticket3", Description: "Description of the task 3", ID: 3},
 	}
 
 	inProgressTickets := templates.Tickets{
-		templates.Ticket{"Ticket1 in progress", "Task 1 - currently in progress", 4},
-		templates.Ticket{"Ticket2 in progress", "Task 2 - currently in progress", 5},
-		templates.Ticket{"Ticket3 in progress", "Task 3 - currently in progress", 6},
+		templates.Ticket{Title: "Ticket1 in progress", Description: "Task 1 - currently in progress", ID: 4},
+		templates.Ticket{Title: "Ticket2 in progress", Description: "Task 2 - currently in progress", ID: 5},
+		templates.Ticket{Title: "Ticket3 in progress", Description: "Task 3 - currently in progress", ID: 6},
 	}
 
 	doneTickets := templates.Tickets{
-		templates.Ticket{"Ticket1 done", "Task 1 - already done", 7},
-		templates.Ticket{"Ticket2d done", "Task 2 - already done", 8},
-		templates.Ticket{"Ticket3d done", "Task 3 - already done", 9},
+		templates.Ticket{Title: "Ticket1 done", Description: "Task 1 - already done", ID: 7},
+		templates.Ticket{Title: "Ticket2d done", Description: "Task 2 - already done", ID: 8},
+		templates.Ticket{Title: "Ticket3d done", Description: "Task 3 - already done", ID: 9},
 	}
 
-	return templates.Tasks{toDoTickets, inProgressTickets, doneTickets}
+	return templates.Tasks{ToDo: toDoTickets, InProgress: inProgressTickets, Done: doneTickets}
 }
