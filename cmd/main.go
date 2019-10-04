3549253529f5af4d64bbc02d16888b772f71eee7
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-10-04 10:55:20 +0200

    Implement html templates + CSS file

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
+	"webserver/server/templates"
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
-}
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	tmpl := template.Must(template.ParseFiles("../server/html/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		toDoTickets := templates.Tickets{
+			templates.Ticket{"T1", "T1desc"},
+			templates.Ticket{"T2", "T2desc"},
+			templates.Ticket{"T3", "T3desc"},
+		}
+
+		inProgressTickets := templates.Tickets{
+			templates.Ticket{"T1ip", "T1descip"},
+			templates.Ticket{"T2ip", "T2descip"},
+			templates.Ticket{"T3ip", "T3descip"},
+		}
+
+		doneTickets := templates.Tickets{
+			templates.Ticket{"T1d", "T1descd"},
+			templates.Ticket{"T2d", "T2descd"},
+			templates.Ticket{"T3d", "T3descd"},
+		}
+
+		tasks := templates.Tasks{toDoTickets, inProgressTickets, doneTickets}
+
+		tmpl.Execute(w, tasks)
+
+	})
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("../server/html/style/"))))
+
+	http.ListenAndServe(":8080", nil)
 }
