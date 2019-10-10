d005847c9a8745d4b7c06ce9a809d549c403bf26
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-10-10 10:38:56 +0200

    Update querries and db structure

--- repository/init.go
+++ repository/init.go
@@ -1,11 +1,18 @@
 package repository
 
 // Query to setup the database
-const initState = `CREATE TABLE IF NOT EXISTS 'tasks' (
-						'title' 	VARCHAR(64),
+const initState = `CREATE TABLE IF NOT EXISTS 'columns' (
+							'name' 		VARCHAR(16),
+							'columnID'  VARCHAR(16)
+							) ;`
+
+// Query create a new ticket
+const newColumn = `CREATE TABLE IF NOT EXISTS 'ticket' (
+						'title'		VARCHAR(64),
 						'desc'  	VARCHAR(256), 
 						'state' 	VARCHAR(64),
 						'deadline' 	VARCHAR(16),
 						'priority'  INT,
-						'id'    	VARCHAR(16) 
+						'id'    	VARCHAR(16),
+						'columnID'  VARCHAR(16)
 						) ;`
