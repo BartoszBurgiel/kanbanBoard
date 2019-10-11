e4fba301594bb1ce23436e4ba92f0672e18d9171
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-10-11 15:14:20 +0200

    Refractor handling user input

--- server/action.go
+++ server/action.go
@@ -1,15 +1,98 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"kanbanBoard/core"
+	"net/http"
+	"strconv"
 
-func (s *Server) handleMoveTicket(w http.ResponseWriter, r *http.Request) {
+	"github.com/google/uuid"
+)
 
+func (s *Server) handleMoveTicket(w http.ResponseWriter, r *http.Request, board *core.Board) {
+	ticketID := r.FormValue("ticketID")
+	ticktetState := r.FormValue("currentState")
+	destinationID := r.FormValue("destination")
+
+	fmt.Println("id", ticketID)
+	fmt.Println("ticketState", ticktetState)
+	fmt.Println("destinationID", destinationID)
+
+	state, err := board.GetState(ticktetState)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	destinationState, err := board.GetState(destinationID)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if destinationState.AllowsNewTicket() {
+
+		// Update database
+		changedTicket, err := state.GetTicket(ticketID)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		changedTicket.StateID = destinationID
+
+		error := s.repo.PushTicketToTheDatabase(changedTicket)
+		if error != nil {
+			fmt.Println(error)
+		}
+
+		state.MoveTicketToNewState(destinationState, ticketID)
+
+	} else {
+		fmt.Println("LIMIT ERROR!")
+	}
 }
 
-func (s *Server) handleAddTicket(w http.ResponseWriter, r *http.Request) {
+func (s *Server) handleAddTicket(w http.ResponseWriter, r *http.Request, board *core.Board) {
+	fmt.Println("New ticket:")
 
+	title := r.FormValue("newTitle")
+	desc := r.FormValue("newDescription")
+	date := r.FormValue("newDate")
+	priority := r.FormValue("newPriority")
+	stateID := r.FormValue("newState")
+
+	fmt.Println("title: ", title)
+	fmt.Println("desc: ", desc)
+	fmt.Println("date: ", date)
+	fmt.Println("priority", priority)
+	fmt.Println("stateID", stateID)
+
+	state, err := board.GetState(stateID)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Format prority
+	priorityN, err := strconv.Atoi(priority)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	newTicket := core.TicketElement{
+		Title:       title,
+		Description: desc,
+		Deadline:    date,
+		Priority:    priorityN,
+		ID:          uuid.New().String(),
+		StateID:     stateID,
+	}
+
+	state.AddNewTicket(newTicket)
+
+	error := s.repo.PushTicketToTheDatabase(newTicket)
+	if error != nil {
+		fmt.Println(error)
+	}
 }
 
-func (s *Server) handleAddColumn(w http.ResponseWriter, r *http.Request) {
+func (s *Server) handleAddColumn(w http.ResponseWriter, r *http.Request, board *core.Board) {
 
 }
--- server/handleUserInput.go
+++ server/handleUserInput.go
@@ -1,106 +1,22 @@
 package server
 
 import (
-	"fmt"
-	"kanbanBoard/core"
 	"net/http"
-	"strconv"
-
-	"github.com/google/uuid"
 )
 
 func (s *Server) handleUserInput(w http.ResponseWriter, r *http.Request) {
+	board := s.engine.GetBoard()
 
 	switch r.FormValue("action") {
 	case "move-ticket":
-		// s.handleMove(w, r)
-		fmt.Println("works!")
-	}
-
-	board := s.engine.GetBoard()
-
-	if r.FormValue("ticketID") != "" {
-		ticketID := r.FormValue("ticketID")
-		ticktetState := r.FormValue("currentState")
-		destinationID := r.FormValue("destination")
-
-		fmt.Println("id", ticketID)
-		fmt.Println("ticketState", ticktetState)
-		fmt.Println("destinationID", destinationID)
-
-		state, err := board.GetState(ticktetState)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		destinationState, err := board.GetState(destinationID)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if destinationState.AllowsNewTicket() {
-
-			// Update database
-			changedTicket, err := state.GetTicket(ticketID)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			changedTicket.StateID = destinationID
-
-			error := s.repo.PushTicketToTheDatabase(changedTicket)
-			if error != nil {
-				fmt.Println(error)
-			}
-
-			state.MoveTicketToNewState(destinationState, ticketID)
-
-		} else {
-			fmt.Println("LIMIT ERROR!")
-		}
-
-	} else {
-		fmt.Println("New ticket:")
-
-		title := r.FormValue("newTitle")
-		desc := r.FormValue("newDescription")
-		date := r.FormValue("newDate")
-		priority := r.FormValue("newPriority")
-		stateID := r.FormValue("newState")
-
-		fmt.Println("title: ", title)
-		fmt.Println("desc: ", desc)
-		fmt.Println("date: ", date)
-		fmt.Println("priority", priority)
-		fmt.Println("stateID", stateID)
-
-		state, err := board.GetState(stateID)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// Format prority
-		priorityN, err := strconv.Atoi(priority)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		newTicket := core.TicketElement{
-			Title:       title,
-			Description: desc,
-			Deadline:    date,
-			Priority:    priorityN,
-			ID:          uuid.New().String(),
-			StateID:     stateID,
-		}
-
-		state.AddNewTicket(newTicket)
-
-		error := s.repo.PushTicketToTheDatabase(newTicket)
-		if error != nil {
-			fmt.Println(error)
-		}
-
+		s.handleMoveTicket(w, r, board)
+		break
+	case "add-ticket":
+		s.handleAddTicket(w, r, board)
+		break
+	case "add-column":
+		s.handleAddColumn(w, r, board)
+		break
 	}
 
 	// Reset engine data
